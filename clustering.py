bab9f5dd852244fb79ba83a9dac0930521761292
Ratneshdtdc <146315467+Ratneshdtdc@users.noreply.github.com>
2024-02-15 16:59:58 +0530

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -2,15 +2,33 @@ import streamlit as st
 import pandas as pd
 
 st.title(':blue[Branch Clustering Model]')
-df = pd.read_csv("")
-branch_names = 
+df = pd.read_excel("Performance Table.xlsx")
+df_agglo =  pd.read_csv("yhat_agglo.csv")
+df_kmeans = pd.read_csv("yhat_kmeans.csv")
+df_gmm = pd.read_csv("yhat_gmm.csv")
 
-option = st.selectbox(
+branch_names = df['Dlv Branch Code_'].unique()
+branch_names_t = tuple(branch_names)
+
+option_br = st.selectbox(
    "Select Branch Name/Code.",
-   ("Email", "Home phone", "Mobile phone"),
+   branch_names_t,
    index=None,
    placeholder="Select Branch Name/Code...",
 )
 
-st.write('You selected:', option)
+st.write('You selected:', option_br)
+
+option_model = st.selectbox(
+   "Select AI/ML Model.",
+   ("Agglomerative/Hierarchical Clustering", "GMM: Gaussian Mixture Model Clustering", "KNN: K-Nearest Neighbours Clustering"),
+   index=None,
+   placeholder="Select ML Model...",
+)
+
+st.write('You selected:', option_model)
+
+
+
+
 
