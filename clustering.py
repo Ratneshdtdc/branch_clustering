d3233325cbd96b56c3415f8602bd1593fcbe3fb0
Ratneshdtdc <146315467+Ratneshdtdc@users.noreply.github.com>
2024-02-16 10:38:57 +0530

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -70,11 +70,12 @@ st.write("Chosen ", Cluster_no, 'Cluster Number...')
 st.write("Showing Branches with belong to the cluster")
 
 if option_model  == "Agglomerative/Hierarchical Clustering":
-   st.dataframe(df_agglo_fin[df_agglo_fin['cluster'] == 5 - Cluster_no])
+   st.write("mmmmm")
+   st.dataframe(df_agglo_fin[df_agglo_fin['cluster'] == (5 - Cluster_no)])
 if option_model  == "GMM: Gaussian Mixture Model Clustering":
-   st.dataframe(df_gmm_fin[df_gmm_fin['cluster'] == 5 - Cluster_no])
+   st.dataframe(df_gmm_fin[df_gmm_fin['cluster'] == (5 - Cluster_no)])
 if option_model  == "KNN: K-Nearest Neighbours Clustering":
-   st.dataframe(df_kmeans_fin[df_kmeans_fin['cluster'] == 5 - Cluster_no])
+   st.dataframe(df_kmeans_fin[df_kmeans_fin['cluster'] == (5 - Cluster_no)])
 #else:
 # pass
    
